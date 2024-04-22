a67343e628db320e47d5b5790d13e1f0637fca10
Oscar Lewis <1351014+OscarLewis@users.noreply.github.com>
2024-04-22 11:55:31 -0700

    add about link to create a page for later

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,9 +2,18 @@
 </script>
 
 <main>
-  <div class="flex flex-row items-baseline mt-1 mb-2 ml-2">
-    <div class="text-2xl">Old Vashon</div>
-    <div class="text-xl"><p>&nbsp;- A historic mapping project</p></div>
+  <div class="flex flex-row justify-between items-baseline mt-1 mb-2">
+    <div class="flex flex-row items-baseline ml-2 font-serif">
+      <div class="text-2xl">Old Vashon</div>
+      <div class="text-xl"><p>&nbsp;- A history mapping project</p></div>
+    </div>
+    <div class="mr-2">
+      <a
+        href="https://github.com/OscarLewis/oldvashon"
+        class="bluelinkcolor"
+        target="_blank">About</a
+      >
+    </div>
   </div>
 </main>
 
