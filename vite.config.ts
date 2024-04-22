0f19df10bc3b281e92971bdefef870b136eeb1bb
Oscar Lewis <1351014+OscarLewis@users.noreply.github.com>
2024-04-22 00:01:51 -0700

    add await comment to mdn

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   esbuild: {
     supported: {
       "top-level-await": true, //browsers can handle top-level-await features
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await
     },
   },
   build: { chunkSizeWarningLimit: 10000 },
