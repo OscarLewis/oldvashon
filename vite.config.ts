36129772af4d22b75451c8244c15c378d980319d
Oscar Lewis <1351014+OscarLewis@users.noreply.github.com>
2024-04-23 14:04:17 -0700

    no more svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [],
   esbuild: {
     supported: {
       "top-level-await": true, //browsers can handle top-level-await features
