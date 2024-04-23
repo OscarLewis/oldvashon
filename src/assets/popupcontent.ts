3eb2399724caed2fed47ebbe9fdc3cea16b4eb14
Oscar Lewis <1351014+OscarLewis@users.noreply.github.com>
2024-04-23 12:52:36 -0700

    update popup width as content changes

--- src/assets/popupcontent.ts
+++ src/assets/popupcontent.ts
@@ -1,25 +1,60 @@
 import { marked } from "marked";
 import type { FeatureLike } from "ol/Feature";
 
+// Make all links target "_blank"
+let renderer = new marked.Renderer();
+renderer.link = function (href, title, text) {
+  let link = marked.Renderer.prototype.link.call(this, href, title, text);
+  return link.replace("<a", "<a target='_blank' ");
+};
+
+// Set new renderer options
+marked.setOptions({
+  renderer: renderer,
+});
+
+// Function that creates the contents for a popup on the map - returns a string
 export function popupContents(feature: FeatureLike): string {
+  let popupDiv = document.querySelector(".ol-popup");
   let popupContent = "<div>";
   // Name
-  if (feature.get("name") != null) {
-    popupContent += "<p class='text-xl'>" + feature.get("name") + "</p>";
+  if (feature.get("name") != null && feature.get("name") != undefined) {
+    popupContent +=
+      "<div class='flex'><p class='text-xl mx-auto'>" +
+      feature.get("name") +
+      "</p></div>";
   }
 
-  // Author
-  if (feature.get("author") != null) {
-    popupContent +=
-      "<p class='text-sm'>" + "Author: " + feature.get("author") + "</p>";
+  //   popupContent += "<div class='flex flex-row'>";
+
+  // Image
+  if (feature.get("img_url") != null && feature.get("img_url") != undefined) {
+    popupDiv?.classList.remove("ol-popup-min-width");
+    popupDiv?.classList.add("min-w-[400px]");
+    popupContent += "<img class='h-[200px] my-2 mx-auto' src='";
+    popupContent += feature.get("img_url");
+    popupContent += "'";
+    popupContent += " >";
+  } else {
+    if (!popupDiv?.classList.contains("ol-popup-min-width")) {
+      popupDiv?.classList.add("ol-popup-min-width");
+    }
   }
 
   // History
   if (feature.get("history") != null) {
-    popupContent += "<div class='markdown text-sm'>";
+    popupContent += "<div class='markdown text-sm markdownlinkcolor'>";
     popupContent += marked(feature.get("history"));
     popupContent += "</div>";
   }
+
+  //   popupContent += "</div>";
+
+  // Author
+  //   if (feature.get("author") != null) {
+  //     popupContent +=
+  //       "<p class='text-sm'>" + "Author: " + feature.get("author") + "</p>";
+  //   }
 
   popupContent += "</div>";
   return popupContent;
