9c69129fdad4038f2ae291514713a4da8abaddec
Oscar Lewis <1351014+OscarLewis@users.noreply.github.com>
2024-04-26 14:40:08 -0700

    add schema sql files

---
+++ qgis/images.sql
@@ -0,0 +1,14 @@
+CREATE TABLE images (
+ imageid INTEGER NOT NULL PRIMARY KEY,
+ feature_id INTEGER NOT NULL,
+ feature_name TEXT NOT NULL,
+ image_url TEXT,
+ image_descrip TEXT,
+ image_attribution TEXT,
+ CONSTRAINT image_id_feature_id
+  FOREIGN KEY (feature_id)
+  REFERENCES vashon_points (feature_id),
+  CONSTRAINT image_name_feature_name
+  FOREIGN KEY (feature_name)
+  REFERENCES vashon_points (feature_name)
+);
---
+++ qgis/vashon_points.sql
@@ -0,0 +1,29 @@
+CREATE TABLE
+    vashon_points (
+        pk,
+        INTEGER NOT NULL PRIMARY KEY,
+        feature_id INTEGER NOT NULL,
+        feature_name TEXT NOT NULL,
+        description TEXT,
+        history TEXT,
+        start_year DATE,
+        end_year DATE,
+        long_name TEXT,
+        author TEXT,
+        summary TEXT,
+        last_edited DATE,
+        citations TEXT
+    );
+
+-- Add Geometry Column to vashon_points
+SELECT
+    AddGeometryColumn ('vashon_points', 'geom', 2285, 'POINT', 'XY');
+
+-- Add support for SpatialIndex-Queries
+SELECT
+    CreateSpatialIndex (
+        -- the table-name
+        'vashon_points',
+        -- the geometry column-name
+        'geom'
+    );
