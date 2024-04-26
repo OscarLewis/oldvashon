f3516fa012c3f2229f61a374b6e2b34ba9f2f3e3
Oscar Lewis <1351014+OscarLewis@users.noreply.github.com>
2024-04-26 15:16:42 -0700

    update schema

--- qgis/images.sql
+++ qgis/images.sql
@@ -1,11 +1,10 @@
 CREATE TABLE
   images (
     imageid INTEGER NOT NULL PRIMARY KEY,
-    feature_id INTEGER NOT NULL,
-    feature_name TEXT NOT NULL,
+    -- These feature keys match the vashon_points table, if I make them actual foreign keys than qgis bugs out
+    feature_id INTEGER,
+    feature_name TEXT,
     image_url TEXT,
     image_descrip TEXT,
-    image_attribution TEXT,
-    CONSTRAINT image_id_feature_id FOREIGN KEY (feature_id) REFERENCES vashon_points (feature_id),
-    CONSTRAINT image_name_feature_name FOREIGN KEY (feature_name) REFERENCES vashon_points (feature_name)
+    image_attribution TEXT
   );
