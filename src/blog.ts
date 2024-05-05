d6eb17152ad86f3f8588605e6cf634bd813b6ffa
Oscar Lewis <1351014+OscarLewis@users.noreply.github.com>
2024-05-05 15:37:34 -0700

    update blog to use _self as the TOC link target

--- src/blog.ts
+++ src/blog.ts
@@ -3,5 +3,15 @@ import renderMD from "./rendermarkdown";
 import blogTOC from "./markdown_files/blog-toc.md";
 import blogMD from "./markdown_files/blog.md";
 
-renderMD(blogTOC, "blog-toc-markdown");
+await renderMD(blogTOC, "blog-toc-markdown").then(() => {
+  /* Since we changed the default of marked to use _blank as the link target
+    we change every link to the Table of Contents to use _self*/
+  const blogTOCDiv = document.getElementById("blog-toc-markdown");
+  if (blogTOCDiv != null) {
+    let blogTOCLinks = blogTOCDiv.getElementsByTagName("a");
+    for (let i = 0; i < blogTOCLinks.length; i++) {
+      blogTOCLinks[i].setAttribute("target", "_self");
+    }
+  }
+});
 renderMD(blogMD, "blog-markdown");
